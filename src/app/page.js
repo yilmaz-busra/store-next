da912d975c284caa64bc141c810c729395e64c0c
busra <b.busraayilmaz@gmail.com>
2024-04-26 01:06:20 +0300

    banner added to page

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,21 @@ import { allProducts } from "@/api/endpoint";
 export default async function Home() {
   const products = await allProducts();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {JSON.stringify(products)}
+    <main className="flex flex-col min-h-screen  ">
+      <section className="relative w-full h-[650px] mb-12   ">
+        <img
+          src="https://audiophile-ecommerce-website.vercel.app/assets/home/desktop/image-hero.jpg"
+          alt="logo"
+          className="absolute min-w-full h-full object-cover"
+        />
+        <div className=" absolute w-full h-full text-white ">
+          <div className="w-1/2 h-full flex flex-col justify-center px-20 ">
+            <h1>NEW PRODUCT</h1>
+            <p>Experience our new product that are already loved by others</p>
+          </div>
+        </div>
+      </section>
+      {/* {JSON.stringify(products)} */}
     </main>
   );
 }
