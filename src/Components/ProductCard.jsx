6330c2a0862cbf7112c9e7f815a94a349e854565
busra <b.busraayilmaz@gmail.com>
2024-06-09 22:10:44 +0300

    addItem imple

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,23 +1,33 @@
+"use client";
+import { useBasket } from "@/context/basketContext";
 import React from "react";
 
-function ProductCard({ image, price, pname }) {
+function ProductCard({ item }) {
+  const { addItem } = useBasket();
   return (
     <div className="xl:w-1/4 md:w-1/2 p-4">
       <div class="bg-[#F1F1F1] p-6 rounded-lg">
         <img
           className="h-40 rounded w-full object-contain object-center mb-6"
-          src={image}
+          src={item.image}
           alt="content"
         />
 
         <div className="flex flex-wrap justify-between items-center w-full">
           <h2 className=" text-lg text-gray-900 font-medium title-font mb-4">
-            {pname.length > 30 ? pname.slice(0, 20) + "..." : pname}
+            {item.title.length > 30
+              ? item.title.slice(0, 20) + "..."
+              : item.title}
           </h2>
-          <h3 className=" text-xs mb-2 font-medium title-font">{price}</h3>
+          <h3 className=" text-xs mb-2 font-medium title-font">{item.price}</h3>
         </div>
 
-        <button className="w-full rounded-full  border-2  text-white bg-neutral-900 hover:bg-white hover:text-neutral-900 ">
+        <button
+          className="w-full rounded-full  border-2  text-white bg-neutral-900 hover:bg-white hover:text-neutral-900 "
+          onClick={() => {
+            addItem(item);
+          }}
+        >
           Buy Now
         </button>
       </div>
