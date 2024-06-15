e3dd0066efb4bbd3cd978a6bb0addac87a67f628
busra <b.busraayilmaz@gmail.com>
2024-06-15 19:05:12 +0300

    minor change

--- src/app/basket/page.js
+++ src/app/basket/page.js
@@ -6,28 +6,28 @@ function Basket() {
   const total = basket.reduce((acc, item) => acc + item.price, 0);
   return (
     <div>
-      <section class="text-gray-600 body-font">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-col text-center w-full mb-20">
-            <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">
               Your Basket
             </h1>
           </div>
-          <div class="lg:w-2/3 w-full mx-auto overflow-auto">
-            <table class="table-auto w-full text-left whitespace-no-wrap">
+          <div className="lg:w-2/3 w-full mx-auto overflow-auto">
+            <table className="table-auto w-full text-left whitespace-no-wrap">
               <thead>
                 <tr>
-                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
                     Product
                   </th>
 
                   {/* <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                     Count
                   </th> */}
-                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                     Price
                   </th>
-                  <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
+                  <th className="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
                     Remove
                   </th>
                 </tr>
@@ -36,12 +36,12 @@ function Basket() {
                 {basket.map((item) => {
                   return (
                     <tr key={item.id}>
-                      <td class="px-4 py-3">{item.title}</td>
+                      <td className="px-4 py-3">{item.title}</td>
                       {/* <td class="px-4 py-3">5 Mb/s</td> */}
-                      <td class="px-4 py-3">{item.price}</td>
-                      <td class="w-10 text-center">
+                      <td className="px-4 py-3">{item.price}</td>
+                      <td className="w-10 text-center">
                         <button
-                          class="text-white bg-red-500 border-0 py-2 px-4 focus:outline-none hover:bg-red-600 rounded"
+                          className="text-white bg-red-500 border-0 py-2 px-4 focus:outline-none hover:bg-red-600 rounded"
                           onClick={() => {
                             removeItem(item.id);
                           }}
@@ -55,8 +55,8 @@ function Basket() {
               </tbody>
             </table>
           </div>
-          <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
-            <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+          <div className="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
+            <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
               Total: {total}$
               <svg
                 fill="none"
@@ -70,7 +70,7 @@ function Basket() {
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
             </a>
-            <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+            <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
               Buy NOW
             </button>
           </div>
